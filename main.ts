f9617c2ee7a16d9bfa2b07d677cca9ad2ec8fd30
Davnox900 <68035597+Davnox900@users.noreply.github.com>
2020-07-16 14:35:44 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,17 @@
-let x = 0
 let y = 0
+let intensité = 0
+let x = 0
 basic.forever(function () {
-    led.plot(x, y)
+    y = 0
+    intensité = 255
+    x = randint(0, 4)
+    basic.pause(200)
+    for (let index = 0; index < 7; index++) {
+        led.plot(x, y)
+        basic.pause(200)
+        y += 1
+        intensité += -40
+        led.setBrightness(intensité)
+        led.unplot(x, y - 3)
+    }
 })
